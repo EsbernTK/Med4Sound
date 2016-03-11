487b8b1f2c6ff6652a5d1f7637aead22e5c28f3a
nstovring <nstovring@live.dk>
2016-03-11 11:47:26 +0100

    ASdasdas

---
+++ Med4Sound/Assets/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class TestScript : MonoBehaviour{
+    KinectSensor kinectSensor;
+    Windows.Kinect.AudioSource aSource;
+    // Use this for initialization
+    void Start () {
+        kinectSensor = KinectSensor.GetDefault();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(kinectSensor.AudioSource.AudioBeams[0].BeamAngle + " beamangle 1");
+        aSource = kinectSensor.AudioSource;
+
+        Debug.Log(aSource.AudioBeams.Count);
+         //Debug.Log(kinectSensor.AudioSource.AudioBeams[1].BeamAngle + " beamangle 2");
+    }
+}
